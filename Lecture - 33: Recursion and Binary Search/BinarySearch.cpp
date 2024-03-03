961b8ec77cf2bc1cb2fa1faa84e1fb8d5c21b963
Chaitanya <58528655+chaitanyanalage@users.noreply.github.com>
2024-03-02 22:33:53 -0800

    Binary Search  using recursion

---
+++ Lecture - 33: Recursion and Binary Search/BinarySearch.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+bool binarySearch(int arr[], int start, int end, int key){
+    //base case
+
+    //element not found
+    if(start > end)
+        return false;
+    
+    int mid = start + (end - start) / 2;
+
+    //element found
+    if(arr[mid] == key)
+        return true;
+
+    if(arr[mid] < key) {
+        return binarySearch(arr, mid + 1, end, key);
+    }
+    else {
+        return binarySearch(arr, start, mid - 1, key);
+    }
+    
+}
+
+int main() {
+    int arr[6] = {2,4,5,10,14,16};
+    int start = 0;
+    int end = 5; 
+    int key = 16;
+
+    bool ans = binarySearch(arr, start, end, key);
+
+    if(ans)
+        cout << "Element is present in array" << endl;
+    else
+        cout << "Element is absent in array" << endl;
+
+}
