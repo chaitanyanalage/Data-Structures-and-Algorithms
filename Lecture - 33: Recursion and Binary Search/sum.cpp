428c642fc898d455f8778c4eb05edb15846b8472
Chaitanya <58528655+chaitanyanalage@users.noreply.github.com>
2024-03-02 21:04:09 -0800

    Calculating sum of array using recursion

---
+++ Lecture - 33: Recursion and Binary Search/sum.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int sum(int arr[], int size) {
+
+    //base case
+    if(size == 0)
+        return 0;
+    
+    //processing
+    int total = arr[0] + sum(arr + 1, size - 1);
+
+    return total;
+}
+
+int main() {
+    int arr[5] = {2,4,9,9,9};
+    int size = 5;
+
+    cout << sum(arr, size) << endl; 
+
+}
