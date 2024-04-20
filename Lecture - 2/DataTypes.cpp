07d79e5af266fdf0ac5a2d875ee8524d00576ef8
Chaitanya <58528655+chaitanyanalage@users.noreply.github.com>
2024-04-20 11:26:20 -0700

    Basics

---
+++ Lecture - 2/DataTypes.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a = 123;
+    cout << "Integer: " << a << endl;
+
+    char b = 'v';
+    cout << "Character: " << b << endl;
+
+    bool c = false;
+    cout << "Boolean: " << c << endl;
+
+    float d = 1.2;
+    cout << "Float: " << d << endl;
+
+    double e = 1.23;
+    cout << "Double: " << e << endl;
+
+}
---
+++ Lecture - 2/SizeOf.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a = 98;
+    cout << "Size of Integer: " << sizeof(a) << endl;
+
+    char b = 'a';
+    cout << "Size of Character: " << sizeof(b) << endl;
+
+    bool c = true;
+    cout << "Size of Boolean: " << sizeof(c) << endl;
+
+    float d = 1.2;
+    cout << "Size of Float: " << sizeof(d) << endl;
+
+    double e = 1.23;
+    cout << "Size of Double: " << sizeof(e) << endl;
+
+
+}
