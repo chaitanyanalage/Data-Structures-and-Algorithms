0542007c543acfe4ef318d16fdf5272c2e373f99
Chaitanya <58528655+chaitanyanalage@users.noreply.github.com>
2024-06-06 23:13:00 -0700

    Time: 11 ms (12.82%), Space: 23.6 MB (11.56%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        s = s + " ";
+        string ans, temp;
+
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == ' ') {
+                if(temp != "") {
+                    ans = temp + " " + ans;
+                }
+                temp = "";
+            }
+            else {
+                temp = temp + s[i];
+            }
+        }
+        return ans.substr(0 , ans.size() - 1);
+    }
+};
