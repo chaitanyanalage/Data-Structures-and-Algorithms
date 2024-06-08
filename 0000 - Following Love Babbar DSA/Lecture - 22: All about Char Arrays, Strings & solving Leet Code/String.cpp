f7ca27b1920fea253fec85f20991d14d3de7954d
Chaitanya <58528655+chaitanyanalage@users.noreply.github.com>
2024-06-08 00:27:12 -0700

    String

--- 0000 - Following Love Babbar DSA/Lecture - 22: All about Char Arrays, Strings & solving Leet Code/String.cpp
+++ 0000 - Following Love Babbar DSA/Lecture - 22: All about Char Arrays, Strings & solving Leet Code/String.cpp
@@ -1,6 +1,56 @@
 #include<iostream>
 using namespace std;
 
+char getMacOccCharacter(string s) {
+    int arr[26] = {0};
+
+    for(int i = 0; i < s.size(); i++) {
+        char ch = s[i];
+        int number = 0;
+        if(ch >= 'a' && ch <= 'z') {
+            number = ch - 'a'; 
+        }
+        else {
+            number = ch - 'A';
+        }
+        arr[number]++;
+    }
+
+    int maxi = -1, ans = 0;
+    for(int i = 0; i < 26; i++) {
+        if(maxi < arr[i]) {
+            ans = i;
+            maxi = arr[i];
+        }
+    }
+    
+    char finalChar = 'a' + ans;
+
+    return finalChar;
+}
+
+char toLowerCase(char ch) {
+    if(ch >= 'a' && ch <= 'z') {
+        return ch;
+    }
+    else {
+        char temp = ch - 'A' + 'a';
+        return temp;
+    }
+}
+
+bool checkPalindrome(char name[], int n) {
+    int s = 0;
+    int e = n - 1;
+
+    while(s < e) {
+        if(toLowerCase(name[s++]) != toLowerCase(name[e--])) {
+            return false;
+        }
+    }
+    return true;
+}
+
 void reverse(char name[], int n) {
     int s = 0;
     int e = n - 1;
@@ -39,6 +89,14 @@ int main() {
     cout << "Reverse of string is ";
     cout << name;
 
+    cout << "Palindrome or Not Palindrome: ";
+    cout << checkPalindrome(name, len) << endl;
 
+    cout << "Character is " << toLowerCase('a') << endl;
+    cout << "Character is " << toLowerCase('A') << endl;
+
+    cout << "Maximum occuring character is ";
+    cout << getMacOccCharacter(name) << endl;
+ 
     return 0;
 }
