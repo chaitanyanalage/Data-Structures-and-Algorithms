495437e6bceb155e6485b50ce325d54e2a2b1ab0
Chaitanya <58528655+chaitanyanalage@users.noreply.github.com>
2024-08-08 20:17:41 -0700

    OOPS concepts

--- 0000 - Following Love Babbar DSA/Lecture - 42: OOPs Concepts/intro.cpp
+++ 0000 - Following Love Babbar DSA/Lecture - 42: OOPs Concepts/intro.cpp
@@ -9,10 +9,45 @@ using namespace std;
         int health;
 
         public:
+        char *name;
         char level;
 
+        Hero() {
+            cout << endl;
+            cout << "Simple counstructor called" << endl;
+            name = new char[100];
+            cout << endl;
+        }
+
+        //Paramerterised Constructor
+        Hero(int health) {
+            //cout << "this -> " << this << endl;
+            this -> health = health;
+
+            cout << "health: " << health << endl;
+        }
+
+        Hero(int health, char level) {
+            this -> level = level;
+            this -> health = health;    
+        }
+
+        //copy constructor
+        Hero(Hero& temp) {
+            char *ch = new char[strlen(temp.name) + 1];
+            strcpy(ch, temp.name);
+            this->name = ch;
+
+            cout << "Copy constructor called " << endl; 
+            this->health = temp.health;
+            this->level = temp.level;
+        }
+
         void print() {
-            cout << level << endl;
+            cout << endl;
+            cout << "Name: " << this->name << endl;
+            cout << "Health: " << this->health << endl;
+            cout << "Level: " << this->level << endl;
         }
 
         //getter
@@ -33,11 +68,85 @@ using namespace std;
             level = ch;
         }
 
+        void setName(char name[]) {
+            strcpy(this->name,name);
+        }
+
+        ~Hero() {
+            cout << "Destructor called" << endl;
+        }
 
     };
 
     int main() {
 
+        //Static
+        Hero a;
+
+        //Dynamic
+        Hero *b = new Hero();
+        //For dymanic  we need to call destructor manually
+        delete b;
+
+
+
+        /*
+        Hero hero1;
+        hero1.setHealth(12);
+        hero1.setLevel('D');
+        char name[7] = "Babbar";
+        hero1.setName(name);
+
+        hero1.print();
+
+        //use defult copy constructor
+        Hero hero2(hero1);
+        // Hero hero2 = hero1;
+        hero2.print();
+
+        hero1.name[0] = 'G';
+        hero1.print();
+
+        hero2.print();
+
+        hero1 = hero2;
+
+        hero1.print();
+        hero2.print();
+        */
+
+        /*
+        Hero suresh(70, 'C');
+        // suresh.setHealth(70);
+        // suresh.setLevel('C');
+        suresh.print();
+
+        //Copy constructor
+        Hero ritesh(suresh);
+        // ritesh.health = suresh.health;
+        // ritesh.level = suresh.level;
+        ritesh.print();
+        */
+
+        
+        /*
+        //object crerated statically
+        Hero ramesh(10);
+        cout << "Address of ramesh -> "  << &ramesh << endl;
+        ramesh.print();
+
+        //dynamically
+        Hero *h = new Hero();
+        h -> print();
+
+        //deallocate memory
+        delete h;
+
+        Hero temp(22, 'B');
+        temp.print();
+        */
+
+        /*
         //static allocation
         Hero a;
 
@@ -76,4 +185,5 @@ using namespace std;
 
 
         // return 0;
+        */
     }
