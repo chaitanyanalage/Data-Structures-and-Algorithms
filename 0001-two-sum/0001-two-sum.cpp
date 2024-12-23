92047dc42742be93abccad5a40e9d2ab93f67a40
Chaitanya <58528655+chaitanyanalage@users.noreply.github.com>
2024-12-23 09:48:21 +0530

    Time: 45 ms (24.25%), Space: 14.1 MB (53.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -6,12 +6,10 @@ public:
         for(i = 0; i < nums.size() - 1; i++) {
             for(j = i + 1; j < nums.size(); j++) {
                 if(nums[i] + nums[j] == target) {
-                    goto jump;
+                    return {i,j};
                 }
             }
         }
-
-        jump:
-        return {i,j};
+        return {};
     }
 };
