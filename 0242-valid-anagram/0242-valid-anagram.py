d531d6e0f5d5e8b0c619e987b2a39fac7c291f97
Chaitanya <58528655+chaitanyanalage@users.noreply.github.com>
2025-10-30 11:37:25 -0700

    Time: 15 ms (41.07%), Space: 19 MB (11.75%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,17 +1,5 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        if len(s) != len(t):
-            return False
-        
-        count = [0] * 26
-
-        for i in range(len(s)):
-            count[ord(s[i]) - ord('a')] += 1
-            count[ord(t[i]) - ord('a')] -= 1
-
-        return all(c == 0 for c in count)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        first = sorted(list(s))
+        second = sorted(list(t))
+        return first == second
