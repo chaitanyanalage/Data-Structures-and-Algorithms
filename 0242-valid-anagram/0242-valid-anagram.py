581e8d031d078c05fb2adec7da02b27519ae0e7c
Chaitanya <58528655+chaitanyanalage@users.noreply.github.com>
2025-12-03 21:32:24 -0800

    Time: 7 ms (89.91%), Space: 17.9 MB (62.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -3,10 +3,13 @@ class Solution:
         if len(s) != len(t):
             return False
         
-        countS, countT = {}, {}
+        count = [0] * 26
 
         for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)
-            countT[t[i]] = 1 + countT.get(t[i], 0)
-        
-        return countS == countT
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
+
+        for val in count:
+            if val != 0:
+                return False
+        return True
