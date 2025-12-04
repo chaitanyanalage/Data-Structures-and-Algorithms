2abdf4cbf4d47b01230db02dd90037f406933a66
Chaitanya <58528655+chaitanyanalage@users.noreply.github.com>
2025-12-03 21:23:14 -0800

    Time: 17 ms (19.86%), Space: 18.5 MB (11.15%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -3,12 +3,4 @@ class Solution:
         if len(s) != len(t):
             return False
         
-        count = [0] * 26
-        for i in range(len(s)):
-            count[ord(s[i]) - ord('a')] += 1
-            count[ord(t[i]) - ord('a')] -= 1
-
-        for val in count:
-            if val != 0:
-                return False
-        return True
+        return sorted(s) == sorted(t)
