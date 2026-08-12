7692e68e455f99c77374c1445f9aca52d3b26172
Chaitanya <58528655+chaitanyanalage@users.noreply.github.com>
2026-08-11 21:34:38 -0700

    Time: 4 ms (1.49%), Space: 9.2 MB (11.36%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,24 +1,21 @@
-#include<string>
-
 class Solution {
 public:
     bool isValid(string s) {
-        while(true) {
-            size_t pos = string::npos;
-            if((pos = s.find("()")) != string::npos) {
-                s.erase(pos, 2);
-                continue;
+        stack<char> stack;
+        unordered_map<char, char> closeToOpen = {
+            {')', '('}, {']', '['}, {'}', '{'}};
+
+        for (char c : s) {
+            if (closeToOpen.count(c)) {
+                if (!stack.empty() && stack.top() == closeToOpen[c]) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stack.push(c);
             }
-            if((pos = s.find("{}")) != string::npos) {
-                s.erase(pos, 2);
-                continue;
-            }
-            if((pos = s.find("[]")) != string::npos) {
-                s.erase(pos, 2);
-                continue;
-            }
-            break;
         }
-        return s.empty();
+        return stack.empty();
     }
 };
